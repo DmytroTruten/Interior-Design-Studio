763551c330c200b629e91fdf96fbbfdddb6d0f66
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-01 17:23:21 +0200

    add navigation for remaining links

--- js/index.js
+++ js/index.js
@@ -1,34 +1,46 @@
-const mainButtons = document.querySelectorAll('.main-btn');
-const links = document.querySelectorAll('.navigation-link');
-// const sections = [
-//   document.querySelector('.about-studio-section'),
-//   document.querySelector('.our-services-section'),
-//   document.querySelector('.our-portfolio-section'),
-//   document.querySelector('.our-pricing-section'),
-//   document.querySelector('.testimonials-section'),
-// ]
-const aboutStudioSection = document.querySelector('.about-studio-section');
+const mainButtons = document.querySelectorAll(".main-btn");
+const links = document.querySelectorAll(".navigation-link");
 
+
+
+
+const aboutStudioSection = document.querySelector(".about-studio-section");
 
 mainButtons.forEach((button) => {
-  button.addEventListener("mouseover", event => {
-    event.target.classList.remove("pulsating-effect-off")
-    event.target.classList.add("pulsating-effect-on")
-    event.target.style.animation = "pulsating-effect-on .5s ease-in-out forwards"
-  })
-  button.addEventListener("mouseout", event => {
-    event.target.classList.remove("pulsating-effect-on")
-    event.target.classList.add("pulsating-effect-off")
-    event.target.style.animation = "pulsating-effect-off .5s ease-in-out forwards"
-  })
-})
+  button.addEventListener("mouseover", (event) => {
+    event.target.classList.remove("pulsating-effect-off");
+    event.target.classList.add("pulsating-effect-on");
+    event.target.style.animation =
+      "pulsating-effect-on .5s ease-in-out forwards";
+  });
+  button.addEventListener("mouseout", (event) => {
+    event.target.classList.remove("pulsating-effect-on");
+    event.target.classList.add("pulsating-effect-off");
+    event.target.style.animation =
+      "pulsating-effect-off .5s ease-in-out forwards";
+  });
+});
 
 links.forEach((link) => {
-  link.addEventListener("click", event => {
+  link.addEventListener("click", (event) => {
+    let selectedSection;
     switch (event.target.id) {
       case "about":
-        aboutStudioSection.scrollIntoView({block: "center", behavior: "smooth"})
-        break
+        selectedSection = document.querySelector(".about-studio-section");
+        break;
+      case "services":
+        selectedSection = document.querySelector(".our-services-section");
+        break;
+      case "portfolio":
+        selectedSection = document.querySelector(".our-portfolio-section");
+        break;
+      case "pricing":
+        selectedSection = document.querySelector(".our-pricing-section");
+        break;
+      case "testimonials":
+        selectedSection = document.querySelector(".testimonials-section");
+        break;
     }
-  })
-})
+    selectedSection.scrollIntoView({ block: "start", behavior: "smooth" });
+  });
+});
