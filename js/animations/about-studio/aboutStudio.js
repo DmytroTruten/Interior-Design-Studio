05e0e91001e84477b236c251df437af9655c5b89
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-06 12:19:21 +0200

    fix about-studio function invoking

--- js/animations/about-studio/aboutStudio.js
+++ js/animations/about-studio/aboutStudio.js
@@ -1,20 +1,33 @@
+let wasInvoked = false;
+
 export function aboutStudioReveal() {
   const aboutStudioSectionHeader = document.getElementById(
     "about-studio-section-header"
   );
   const aboutStudioMainText = document.getElementById("about-studio-main-text");
   const aboutStudioMore = document.getElementById("about-studio-more");
+  const aboutStudioMoreContainer = document.querySelector(
+    ".about-studio-more-container"
+  );
 
-  aboutStudioSectionHeader.style.animation = "reveal-to-left 1s ease-in-out";
-  aboutStudioSectionHeader.style.opacity = "1";
+  if (!wasInvoked) {
+    aboutStudioMore.addEventListener("click", () => {
+      aboutStudioMoreContainer.classList.toggle("reveal-text");
+    });
 
-  setTimeout(() => {
-    aboutStudioMainText.style.animation = "reveal-to-right 1s ease-in-out";
-    aboutStudioMainText.style.opacity = "1";
-  }, 500)
+    aboutStudioSectionHeader.style.animation = "reveal-to-left 1s ease-in-out";
+    aboutStudioSectionHeader.style.opacity = "1";
 
-  setTimeout(() => {
-    aboutStudioMore.style.animation = "reveal-to-left 1s ease-in-out";
-    aboutStudioMore.style.opacity = "1";
-  }, 950)
+    setTimeout(() => {
+      aboutStudioMainText.style.animation = "reveal-to-right 1s ease-in-out";
+      aboutStudioMainText.style.opacity = "1";
+    }, 500);
+
+    setTimeout(() => {
+      aboutStudioMore.style.animation = "reveal-to-left 1s ease-in-out";
+      aboutStudioMore.style.opacity = "1";
+    }, 950);
+    
+    wasInvoked = true;
+  }
 }
