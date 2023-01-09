d6b88002e2f0e0719aff70b0b9491e8fa60717a2
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-09 14:49:41 +0200

    animate gallery images

--- js/animations/our-portfolio/ourPortfolio.js
+++ js/animations/our-portfolio/ourPortfolio.js
@@ -11,20 +11,40 @@ export function ourPortfolioReveal(breakpoint) {
       document.getElementById("our-portfolio-office"),
       document.getElementById("our-portfolio-other"),
     ];
+    const galleryImages = [
+      document.getElementById("gallery-img-1"),
+      document.getElementById("gallery-img-2"),
+      document.getElementById("gallery-img-3"),
+      document.getElementById("gallery-img-4"),
+      document.getElementById("gallery-img-5"),
+      document.getElementById("gallery-img-6"),
+      document.getElementById("gallery-img-7"),
+      document.getElementById("gallery-img-8"),
+    ];
     if (breakpoint === "xs") {
       ourPortfolioSectionHeader.style.animation =
         "reveal-to-right 1s ease-in-out";
       ourPortfolioSectionHeader.style.opacity = "1";
-      let time = 200;
-      filters.forEach((filter) => {
+      let filterTime = 200;
+      let galleryImagesTime = 200;
+      for (let filter of filters) {
         setTimeout(() => {
-          filter.style.animation =
-            "reveal-to-top 1s ease-in-out";
+          filter.style.animation = "reveal-to-top 1s ease-in-out";
           filter.style.opacity = "1";
-        }, time);
-        time += 200;
-      });
+        }, filterTime);
+        filterTime += 200;
+      }
+      for (let image of galleryImages) {
+        if(image.id === 'gallery-img-4') {
+          break
+        }
+        setTimeout(() => {
+          image.style.animation = "reveal-to-right 1s ease-in-out";
+          image.style.opacity = "1";
+        }, galleryImagesTime);
+        galleryImagesTime += 200;
+      }
     }
-    wasInvoked = true
+    wasInvoked = true;
   }
 }
