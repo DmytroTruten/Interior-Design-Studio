979d19d84495065d127f37c99d90e12f0765211a
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-11 19:00:32 +0200

    finish animating pricing cards on hover

--- js/animations/our-pricing/pricingCardsHover.js
+++ js/animations/our-pricing/pricingCardsHover.js
@@ -6,70 +6,100 @@ export function pricingCardsHover() {
       document.getElementById("pricing-card-standart"),
       document.getElementById("pricing-card-premium"),
     ];
-    pricingCards[0].addEventListener("mousemove", (event) => {
-      const x = event.pageX - event.currentTarget.offsetLeft;
-      const y = event.pageY - event.currentTarget.offsetTop;
-      if (
-        x < pricingCards[0].offsetWidth / 3 &&
-        y < pricingCards[0].offsetHeight / 3
-      ) {
-        pricingCards[0].style.transform =
-          "perspective(1500px) rotateX(10deg) rotateY(-10deg)";
-      } else if (
-        x > pricingCards[0].offsetWidth / 3 &&
-        x < (pricingCards[0].offsetWidth / 3) * 2 &&
-        y < pricingCards[0].offsetHeight / 3
-      ) {
-        pricingCards[0].style.transform = "perspective(1500px) rotateX(10deg)";
-      } else if (
-        x > (pricingCards[0].offsetWidth / 3) * 2 &&
-        y < pricingCards[0].offsetHeight / 3
-      ) {
-        pricingCards[0].style.transform =
-          "perspective(1500px) rotateX(10deg) rotateY(10deg)";
-      } else if (
-        x < pricingCards[0].offsetWidth / 3 &&
-        y > pricingCards[0].offsetHeight / 3 &&
-        y < (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform = "perspective(1500px) rotateY(-10deg)";
-      } else if (
-        x > pricingCards[0].offsetWidth / 3 &&
-        x < (pricingCards[0].offsetWidth / 3) * 2 &&
-        y > pricingCards[0].offsetHeight / 3 &&
-        y < (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform = "rotateX(0deg) rotateY(0deg)";
-      } else if (
-        x > (pricingCards[0].offsetWidth / 3) * 2 &&
-        y > pricingCards[0].offsetHeight / 3 &&
-        y < (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform = "perspective(1500px) rotateY(10deg)";
-      } else if (
-        x < pricingCards[0].offsetWidth / 3 &&
-        y > (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform =
-          "perspective(1500px) rotateX(-10deg) rotateY(-10deg)";
-      } else if (
-        x > pricingCards[0].offsetWidth / 3 &&
-        x < (pricingCards[0].offsetWidth / 3) * 2 &&
-        y > (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform =
-          "perspective(1500px) rotateX(-10deg)";
-      } else if (
-        x > (pricingCards[0].offsetWidth / 3) * 2 &&
-        y > (pricingCards[0].offsetHeight / 3) * 2
-      ) {
-        pricingCards[0].style.transform =
-          "perspective(1500px) rotateX(-10deg) rotateY(10deg)";
-      }
+    const pricingCardsButtons = [
+      document.getElementById("pricing-card-basic-btn"),
+      document.getElementById("pricing-card-standart-btn"),
+      document.getElementById("pricing-card-premium-btn"),
+    ];
+    pricingCards.forEach((card) => {
+      card.addEventListener("mousemove", (event) => {
+        const cursorPositionX = event.pageX - event.currentTarget.offsetLeft;
+        const cursorPositionY = event.pageY - event.currentTarget.offsetTop;
+        if (
+          cursorPositionX < card.offsetWidth / 3 &&
+          cursorPositionY < card.offsetHeight / 3
+        ) {
+          card.style.transform =
+            "perspective(1500px) rotateX(10deg) rotateY(-10deg)";
+        } else if (
+          cursorPositionX > card.offsetWidth / 3 &&
+          cursorPositionX < (card.offsetWidth / 3) * 2 &&
+          cursorPositionY < card.offsetHeight / 3
+        ) {
+          card.style.transform = "perspective(1500px) rotateX(10deg)";
+        } else if (
+          cursorPositionX > (card.offsetWidth / 3) * 2 &&
+          cursorPositionY < card.offsetHeight / 3
+        ) {
+          card.style.transform =
+            "perspective(1500px) rotateX(10deg) rotateY(10deg)";
+        } else if (
+          cursorPositionX < card.offsetWidth / 3 &&
+          cursorPositionY > card.offsetHeight / 3 &&
+          cursorPositionY < (card.offsetHeight / 3) * 2
+        ) {
+          card.style.transform = "perspective(1500px) rotateY(-10deg)";
+        } else if (
+          cursorPositionX > card.offsetWidth / 3 &&
+          cursorPositionX < (card.offsetWidth / 3) * 2 &&
+          cursorPositionY > card.offsetHeight / 3 &&
+          cursorPositionY < (card.offsetHeight / 3) * 2
+        ) {
+          card.style.transform = "rotateX(0deg) rotateY(0deg)";
+        } else if (
+          cursorPositionX > (card.offsetWidth / 3) * 2 &&
+          cursorPositionY > card.offsetHeight / 3 &&
+          cursorPositionY < (card.offsetHeight / 3) * 2
+        ) {
+          card.style.transform = "perspective(1500px) rotateY(10deg)";
+        } else if (
+          cursorPositionX < card.offsetWidth / 3 &&
+          cursorPositionY > (card.offsetHeight / 3) * 2
+        ) {
+          if (
+            event.target === pricingCardsButtons[0] ||
+            event.target === pricingCardsButtons[1] ||
+            event.target === pricingCardsButtons[2]
+          ) {
+            card.style.transform = "rotateX(0deg) rotateY(0deg)";
+          } else {
+            card.style.transform =
+              "perspective(1500px) rotateX(-10deg) rotateY(-10deg)";
+          }
+        } else if (
+          cursorPositionX > card.offsetWidth / 3 &&
+          cursorPositionX < (card.offsetWidth / 3) * 2 &&
+          cursorPositionY > (card.offsetHeight / 3) * 2
+        ) {
+          if (
+            event.target === pricingCardsButtons[0] ||
+            event.target === pricingCardsButtons[1] ||
+            event.target === pricingCardsButtons[2]
+          ) {
+            card.style.transform = "rotateX(0deg) rotateY(0deg)";
+          } else {
+            card.style.transform = "perspective(1500px) rotateX(-10deg)";
+          }
+        } else if (
+          cursorPositionX > (card.offsetWidth / 3) * 2 &&
+          cursorPositionY > (card.offsetHeight / 3) * 2
+        ) {
+          if (
+            event.target === pricingCardsButtons[0] ||
+            event.target === pricingCardsButtons[1] ||
+            event.target === pricingCardsButtons[2]
+          ) {
+            card.style.transform = "rotateX(0deg) rotateY(0deg)";
+          } else {
+            card.style.transform =
+              "perspective(1500px) rotateX(-10deg) rotateY(10deg)";
+          }
+        }
+      });
+      card.addEventListener("mouseleave", () => {
+        card.style.transform = "rotateX(0deg) rotateY(0deg)";
+      });
     });
-    pricingCards[0].addEventListener('mouseleave', () => {
-      pricingCards[0].style.transform = 'rotateX(0deg) rotateY(0deg)'
-    })
     wasInvoked = true;
   }
 }
