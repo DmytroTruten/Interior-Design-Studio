3ae4446d480dc846ddd3ee7aba6651015f13a98f
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-18 10:51:11 +0200

    translate about-studio section into ua

--- js/main-page/animations/dropdown/dropdown.js
+++ js/main-page/animations/dropdown/dropdown.js
@@ -1,8 +1,8 @@
+import { changeLanguage } from "../../helpers/changeLanguage.js"
 export function dropdownToggle() {
   const selectUALanguage = document.getElementById("ua-language");
   const selectENLanguage = document.getElementById("en-language");
   const dropdownArrow = document.getElementById("dropdown-arrow");
-  const headerLink = document.querySelectorAll(".header-link");
   selectENLanguage.addEventListener("click", () => {
     if (selectUALanguage.classList.contains("language-reveal")) {
       dropdownArrow.style.transform = "rotate(0deg)";
@@ -24,41 +24,4 @@ export function dropdownToggle() {
       changeLanguage("ua");
     }
   });
-
-  function changeLanguage(lang) {
-    location.hash = lang;
-    location.reload();
-  }
-
-  const language = {
-    en: {
-      services: "Services",
-      portfolio: "Portfolio",
-      pricing: "Pricing",
-    },
-    ua: {
-      services: "Послуги",
-      portfolio: "Портфоліо",
-      pricing: "Ціни",
-    },
-  };
-
-  // Check if a hash value exists in the URL
-  if (window.location.hash) {
-    // Set the content of the webpage
-    // depending on the hash value
-    if (window.location.hash === "#ua") {
-      for (let i = 0; i < headerLink.length; i++) {
-        headerLink[i].textContent = Object.values(language.ua)[i];
-      }
-      selectENLanguage.children[0].textContent = "UA";
-      selectUALanguage.children[0].textContent = "EN";
-    } else {
-      for (let i = 0; i < headerLink.length; i++) {
-        headerLink[i].textContent = Object.values(language.en)[i];
-      }
-      selectENLanguage.children[0].textContent = "EN";
-      selectUALanguage.children[0].textContent = "UA";
-    }
-  }
 }
---
+++ js/main-page/helpers/changeLanguage.js
@@ -0,0 +1,86 @@
+const selectUALanguage = document.getElementById("ua-language");
+const selectENLanguage = document.getElementById("en-language");
+const headerLink = document.querySelectorAll(".header-link");
+const heroHeaderH1 = document.getElementById("hero-header-h1");
+const heroHeaderP = document.getElementById("hero-header-p");
+const heroHeaderButton = document.getElementById("hero-header-button");
+const sectionHeaders = document.querySelectorAll('.section-header')
+const aboutStudioSectionHeader = document.getElementById(
+  "about-studio-section-header"
+);
+const aboutStudioMainText = document.getElementById("about-studio-main-text");
+const aboutStudioMoreTextLg = document.getElementById(
+  "about-studio-more-text-lg"
+);
+
+export function changeLanguage(lang) {
+  location.hash = lang;
+  location.reload();
+}
+
+const language = {
+  en: {
+    services: "Services",
+    portfolio: "Portfolio",
+    pricing: "Pricing",
+    heroHeaderH1: "We create your space better",
+    heroHeaderP:
+      "Our team creates comfortable spaces for our clients. We've been designing your everyday life and work through great ideas since 1999.",
+    heroHeaderButton: "GET STARTED",
+    aboutStudioSectionHeader: "About <span>Studio</span>",
+    aboutStudioMainText:
+      "Interiart is an award-winning architecture and interior design practice based in NYC. We work internationally on projects of residential & commercial interior design that require a creative approach. Our talented and experienced designers leverage their knowledge and expertise to create unique and comfortable interiors for you.",
+    aboutStudioMoreTextLg: "<br>Our team knows that interior design can be stressful for the client and we do our best to make it as easy as possible. We listen to your needs, ideas, and inputs. And most importantly, we make it exciting and enjoyable for our clients.",
+  },
+  ua: {
+    services: "Послуги",
+    portfolio: "Портфоліо",
+    pricing: "Ціни",
+    heroHeaderH1: "Ми робимо ваш простір кращим",
+    heroHeaderP:
+      "Наша команда створює комфортний простір для наших клієнтів. З 1999 року ми створюємо ваше повсякденне життя та працюємо над чудовими ідеями.",
+    heroHeaderButton: "РОЗПОЧАТИ",
+    aboutStudioSectionHeader: "Про <span>Студію</span>",
+    aboutStudioMainText: "Interiart – це нагороджена практика архітектури та дизайну інтер’єрів, що базується в Нью-Йорку. Ми працюємо над міжнародними проектами дизайну житлових та комерційних інтер'єрів, які потребують творчого підходу. Наші талановиті та досвідчені дизайнери використовують свої знання та досвід, щоб створити унікальні та комфортні інтер’єри для вас.",
+    aboutStudioMoreTextLg: "<br>Наша команда знає, що дизайн інтер'єру може бути стрессово для клієнта, і ми докладаємо всіх зусиль, щоб зробити його максимально легким. Ми прислухаємося до ваших потреб, ідей та побажань. А головне, ми робимо це захоплюючим і приємним для наших клієнтів.",
+  },
+};
+
+// Check if a hash value exists in the URL
+if (window.location.hash) {
+  // Set the content of the webpage
+  // depending on the hash value
+  if (window.location.hash === "#ua") {
+    for (let i = 0; i < headerLink.length; i++) {
+      headerLink[i].textContent = Object.values(language.ua)[i];
+    }
+    selectENLanguage.children[0].textContent = "UA";
+    selectUALanguage.children[0].textContent = "EN";
+
+    heroHeaderH1.textContent = language.ua.heroHeaderH1;
+    heroHeaderP.textContent = language.ua.heroHeaderP;
+    heroHeaderButton.textContent = language.ua.heroHeaderButton;
+
+    sectionHeaders.forEach(header => {
+      header.classList.add('section-header-ua')
+    })
+
+    aboutStudioSectionHeader.innerHTML = language.ua.aboutStudioSectionHeader
+    aboutStudioMainText.textContent = language.ua.aboutStudioMainText
+    aboutStudioMoreTextLg.innerHTML = language.ua.aboutStudioMoreTextLg
+  } else {
+    for (let i = 0; i < headerLink.length; i++) {
+      headerLink[i].textContent = Object.values(language.en)[i];
+    }
+    selectENLanguage.children[0].textContent = "EN";
+    selectUALanguage.children[0].textContent = "UA";
+
+    heroHeaderH1.textContent = language.en.heroHeaderH1;
+    heroHeaderP.textContent = language.en.heroHeaderP;
+    heroHeaderButton.textContent = language.en.heroHeaderButton;
+
+    aboutStudioSectionHeader.innerHTML = language.en.aboutStudioSectionHeader
+    aboutStudioMainText.textContent = language.en.aboutStudioMainText
+    aboutStudioMoreTextLg.innerHTML = language.en.aboutStudioMoreTextLg
+  }
+}
