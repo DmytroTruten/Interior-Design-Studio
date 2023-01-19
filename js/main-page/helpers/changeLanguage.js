d8135f5fbd2c8b5c7a8beb8a08b2d87b487401bc
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-19 13:46:45 +0200

    adjust language object properties

--- js/main-page/helpers/changeLanguage.js
+++ js/main-page/helpers/changeLanguage.js
@@ -28,7 +28,7 @@ const more = document.querySelectorAll(".more");
 const pricingType = document.querySelectorAll(".pricing-type");
 const perMonth = document.querySelectorAll(".per-month");
 const pricingServices = document.querySelectorAll("#pricing-service");
-const buyNowButton = document.querySelectorAll('.buy-now-button')
+const buyNowButton = document.querySelectorAll(".buy-now-button");
 
 export function changeLanguage(lang) {
   location.hash = lang;
@@ -44,40 +44,30 @@ const language = {
     heroHeaderP:
       "Our team creates comfortable spaces for our clients. We've been designing your everyday life and work through great ideas since 1999.",
     heroHeaderButton: "GET STARTED",
-    sectionHeaders: {
-      aboutStudioSectionHeader: "About <span>Studio</span>",
-      ourServicesSectionHeader: "Our <span>Services</span>",
-      ourPortfolioSectionHeader: "Our <span>Portfolio</span>",
-      ourPricingSectionHeader: "Our <span>Pricing</span>",
-      testimonialsSectionHeader: "What <span>People Say</span>",
-      questionSectionHeader: "Do you have <br><span>any question?</span>",
-    },
+    sectionHeaders: [
+      "About <span>Studio</span>",
+      "Our <span>Services</span>",
+      "Our <span>Portfolio</span>",
+      "Our <span>Pricing</span>",
+      "What <span>People Say</span>",
+      "Do you have <br><span>any question?</span>",
+    ],
     aboutStudioMainText:
       "Interiart is an award-winning architecture and interior design practice based in NYC. We work internationally on projects of residential & commercial interior design that require a creative approach. Our talented and experienced designers leverage their knowledge and expertise to create unique and comfortable interiors for you.",
     aboutStudioMoreTextLg:
       "<br>Our team knows that interior design can be stressful for the client and we do our best to make it as easy as possible. We listen to your needs, ideas, and inputs. And most importantly, we make it exciting and enjoyable for our clients.",
-    ourServicesElements: {
-      interiorDesignCardHeader: "INTERIOR DESIGN",
-      interiorDesignCardDescription:
-        "Interior design services offer a vast variety of solutions for our clients' homes and offices.",
-      decorativeServicesCardHeader: "DECORATIVE SERVICES",
-      decorativeServicesCardDescription:
-        "Our professional decorators will be happy to help you change your house's inner look.",
-      spacePlanningCardHeader: "SPACE PLANNING",
-      spacePlanningCardDescription:
-        "We create better interior experiences through our space planning services.",
-      projectManagementCardHeader: "PROJECT MANAGEMENT",
-      projectManagementCardDescription:
-        "We provide proper project management as it's one of the main success factors in interior design.",
-      consultationButton: "FREE CONSULTATION",
-    },
-    filters: {
-      all: "ALL",
-      commercial: "COMMERCIAL",
-      residential: "RESIDENTIAL",
-      office: "OFFICE",
-      other: "OTHER",
-    },
+    ourServicesElements: [
+      "INTERIOR DESIGN",
+      "Interior design services offer a vast variety of solutions for our clients' homes and offices.",
+      "DECORATIVE SERVICES",
+      "Our professional decorators will be happy to help you change your house's inner look.",
+      "SPACE PLANNING",
+      "We create better interior experiences through our space planning services.",
+      "PROJECT MANAGEMENT",
+      "We provide proper project management as it's one of the main success factors in interior design.",
+      "FREE CONSULTATION",
+    ],
+    filters: ["ALL", "COMMERCIAL", "RESIDENTIAL", "OFFICE", "OTHER"],
     more: "VIEW MORE",
     pricingType: ["Basic", "Standart", "Premium"],
     perMonth: "per month",
@@ -105,40 +95,30 @@ const language = {
     heroHeaderP:
       "Наша команда створює комфортний простір для наших клієнтів. З 1999 року ми створюємо ваше повсякденне життя та працюємо над чудовими ідеями.",
     heroHeaderButton: "РОЗПОЧАТИ",
-    sectionHeaders: {
-      aboutStudioSectionHeader: "Про <span>Студію</span>",
-      ourServicesSectionHeader: "Наші <span>Послуги</span>",
-      ourPortfolioSectionHeader: "Наше <span>Портфоліо</span>",
-      ourPricingSectionHeader: "Наші <span>Ціни</span>",
-      testimonialsSectionHeader: "Що <span>Кажуть Люди</span>",
-      questionSectionHeader: "Маєте <br><span>Запитання?</span>",
-    },
+    sectionHeaders: [
+      "Про <span>Студію</span>",
+      "Наші <span>Послуги</span>",
+      "Наше <span>Портфоліо</span>",
+      "Наші <span>Ціни</span>",
+      "Що <span>Кажуть Люди</span>",
+      "Маєте <br><span>Запитання?</span>",
+    ],
     aboutStudioMainText:
       "Interiart – це нагороджена практика архітектури та дизайну інтер’єрів, що базується в Нью-Йорку. Ми працюємо над міжнародними проектами дизайну житлових та комерційних інтер'єрів, які потребують творчого підходу. Наші талановиті та досвідчені дизайнери використовують свої знання та досвід, щоб створити унікальні та комфортні інтер’єри для вас.",
     aboutStudioMoreTextLg:
       "<br>Наша команда знає, що дизайн інтер'єру може бути стрессово для клієнта, і ми докладаємо всіх зусиль, щоб зробити його максимально легким. Ми прислухаємося до ваших потреб, ідей та побажань. А головне, ми робимо це захоплюючим і приємним для наших клієнтів.",
-    ourServicesElements: {
-      interiorDesignCardHeader: "ДИЗАЙН ІНТЕР'ЄРУ",
-      interiorDesignCardDescription:
-        "Послуги з дизайну інтер’єру пропонують широкий вибір рішень для дому та офісу наших клієнтів.",
-      decorativeServicesCardHeader: "ДЕКОРАТИВНІ ПОСЛУГИ",
-      decorativeServicesCardDescription:
-        "Наші професійні декоратори з радістю допоможуть вам змінити внутрішній вигляд вашого будинку.",
-      spacePlanningCardHeader: "ПЛАНУВАННЯ ПРОСТОРУ",
-      spacePlanningCardDescription:
-        "Ми створюємо кращий інтер’єр за допомогою наших послуг з планування простору.",
-      projectManagementCardHeader: "УПРАВЛІННЯ ПРОЕКТАМИ",
-      projectManagementCardDescription:
-        "Ми забезпечуємо належне управління проектами, оскільки це один із головних факторів успіху в дизайні інтер’єру.",
-      consultationButton: "БЕЗКОШТОВНА КОНСУЛЬТАЦІЯ",
-    },
-    filters: {
-      all: "ВСІ",
-      commercial: "КОМЕРЦІЙНІ",
-      residential: "ЖИТЛОВІ",
-      office: "ОФІСНІ",
-      other: "ІНШІ",
-    },
+    ourServicesElements: [
+      "ДИЗАЙН ІНТЕР'ЄРУ",
+      "Послуги з дизайну інтер’єру пропонують широкий вибір рішень для дому та офісу наших клієнтів.",
+      "ДЕКОРАТИВНІ ПОСЛУГИ",
+      "Наші професійні декоратори з радістю допоможуть вам змінити внутрішній вигляд вашого будинку.",
+      "ПЛАНУВАННЯ ПРОСТОРУ",
+      "Ми створюємо кращий інтер’єр за допомогою наших послуг з планування простору.",
+      "УПРАВЛІННЯ ПРОЕКТАМИ",
+      "Ми забезпечуємо належне управління проектами, оскільки це один із головних факторів успіху в дизайні інтер’єру.",
+      "БЕЗКОШТОВНА КОНСУЛЬТАЦІЯ",
+    ],
+    filters: ["ВСІ", "КОМЕРЦІЙНІ", "ЖИТЛОВІ", "ОФІСНІ", "ІНШІ"],
     more: "БІЛЬШЕ",
     pricingType: ["Базовий", "Стандарт", "Преміум"],
     perMonth: "на місяць",
@@ -179,20 +159,16 @@ if (window.location.hash) {
     aboutStudioMoreTextLg.innerHTML = language.ua.aboutStudioMoreTextLg;
 
     for (let i = 0; i < sectionHeaders.length; i++) {
-      sectionHeaders[i].innerHTML = Object.values(language.ua.sectionHeaders)[
-        i
-      ];
+      sectionHeaders[i].innerHTML = language.ua.sectionHeaders[i];
       sectionHeaders[i].classList.add("section-header-ua");
     }
 
     for (let i = 0; i < ourServicesElements.length; i++) {
-      ourServicesElements[i].textContent = Object.values(
-        language.ua.ourServicesElements
-      )[i];
+      ourServicesElements[i].textContent = language.ua.ourServicesElements[i];
     }
 
     for (let i = 0; i < filters.length; i++) {
-      filters[i].textContent = Object.values(language.ua.filters)[i];
+      filters[i].textContent = language.ua.filters[i];
     }
 
     for (let i = 0; i < more.length; i++) {
@@ -200,15 +176,14 @@ if (window.location.hash) {
     }
 
     for (let i = 0; i < pricingType.length; i++) {
-      pricingType[i].textContent = language.ua.pricingType[i]
-      perMonth[i].textContent = language.ua.perMonth
-      buyNowButton[i].textContent = language.ua.buyNow
+      pricingType[i].textContent = language.ua.pricingType[i];
+      perMonth[i].textContent = language.ua.perMonth;
+      buyNowButton[i].textContent = language.ua.buyNow;
     }
 
     for (let i = 0; i < pricingServices.length; i++) {
       pricingServices[i].textContent = language.ua.pricingServices[i];
     }
-
   } else {
     for (let i = 0; i < headerLink.length; i++) {
       headerLink[i].textContent = Object.values(language.en)[i];
@@ -224,20 +199,16 @@ if (window.location.hash) {
     aboutStudioMoreTextLg.innerHTML = language.en.aboutStudioMoreTextLg;
 
     for (let i = 0; i < sectionHeaders.length; i++) {
-      sectionHeaders[i].innerHTML = Object.values(language.en.sectionHeaders)[
-        i
-      ];
+      sectionHeaders[i].innerHTML = language.en.sectionHeaders[i];
       sectionHeaders[i].classList.remove("section-header-ua");
     }
 
     for (let i = 0; i < ourServicesElements.length; i++) {
-      ourServicesElements[i].textContent = Object.values(
-        language.en.ourServicesElements
-      )[i];
+      ourServicesElements[i].textContent = language.en.ourServicesElements[i];
     }
 
     for (let i = 0; i < filters.length; i++) {
-      filters[i].textContent = Object.values(language.en.filters)[i];
+      filters[i].textContent = language.en.filters[i];
     }
 
     for (let i = 0; i < more.length; i++) {
@@ -249,9 +220,9 @@ if (window.location.hash) {
     }
 
     for (let i = 0; i < pricingType.length; i++) {
-      pricingType[i].textContent = language.en.pricingType[i]
-      perMonth[i].textContent = language.en.perMonth
-      buyNowButton[i].textContent = language.en.buyNow
+      pricingType[i].textContent = language.en.pricingType[i];
+      perMonth[i].textContent = language.en.perMonth;
+      buyNowButton[i].textContent = language.en.buyNow;
     }
 
     for (let i = 0; i < pricingServices.length; i++) {
