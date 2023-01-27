7721e595395c5e52a88fc46886d05654d53e01c9
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-27 16:22:43 +0200

    add redirect for all main-buttons

--- js/main-page/helpers/redirect.js
+++ js/main-page/helpers/redirect.js
@@ -1,11 +1,19 @@
 export function redirect() {
-  const buyNowButtons = document.querySelectorAll('.buy-now-button')
+  const mainButtons = document.querySelectorAll(".main-btn");
 
-  for (let i = 0; i < buyNowButtons.length; i++) {
-    buyNowButtons[i].addEventListener('click', () => {
-      setTimeout(() => {
-        window.location.assign("buySubscription.html")
-      }, 600)
-    })
+  for (let i = 0; i < mainButtons.length; i++) {
+    mainButtons[i].addEventListener("click", () => {
+      if (mainButtons[i].id === "hero-header-button") {
+        window.location.assign("buySubscription.html");
+      } else if (mainButtons[i].id === "consultation-button") {
+        window.location.assign("orderCall.html");
+      } else if (mainButtons[i].classList.contains("buy-now-button")) {
+        setTimeout(() => {
+          window.location.assign("buySubscription.html");
+        }, 600);
+      } else if (mainButtons[i].id === "order-a-call-button") {
+        window.location.assign("orderCall.html");
+      }
+    });
   }
 }
