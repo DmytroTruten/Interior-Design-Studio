9d0f9a2ef8537160d74b45bc3390592bc283dae9
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-27 18:53:16 +0200

    add filter selecting mechanism

--- js/main-page/animations/our-portfolio/filterImages.js
+++ js/main-page/animations/our-portfolio/filterImages.js
@@ -1,20 +1,28 @@
 export function filterPortfolioImages() {
   const filters = document.querySelectorAll("#filter");
-  const galleryImages = document.querySelectorAll('#gallery-img')
-  const commercialImages = document.querySelectorAll('#commercial-img')
+  const galleryImages = document.querySelectorAll("#gallery-img");
+  const commercialImages = document.querySelectorAll("#commercial-img");
+
   for (let i = 0; i < filters.length; i++) {
-    filters[i].addEventListener('click', (event) => {
-      if(event.currentTarget === filters[1]) {
-        galleryImages[0].style.animation = 'hide .5s ease-in-out';
+    filters[i].addEventListener("click", function (event) {
+      let currentFilter = document.getElementsByClassName("active-filter");
+      currentFilter[0].className = currentFilter[0].className.replace(
+        " active-filter",
+        ""
+      );
+      this.className += " active-filter";
+
+      if (event.currentTarget === filters[1]) {
+        galleryImages[0].style.animation = "hide .5s ease-in-out";
         setTimeout(() => {
-          galleryImages[0].style.display = 'none';
-        }, 490)
+          galleryImages[0].style.display = "none";
+        }, 490);
         setTimeout(() => {
-          commercialImages[0].style.display = 'block';
-          commercialImages[0].style.animation = 'reveal .5s ease-in-out';
-          commercialImages[0].style.opacity = '1';
-        }, 500)
+          commercialImages[0].style.display = "block";
+          commercialImages[0].style.animation = "reveal .5s ease-in-out";
+          commercialImages[0].style.opacity = "1";
+        }, 500);
       }
-    })
+    });
   }
 }
