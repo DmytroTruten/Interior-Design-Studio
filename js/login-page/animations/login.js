de48cea560b486f5237098b3b52aff0db892be7e
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-29 17:49:45 +0200

    style and animated login page

---
+++ js/login-page/animations/login.js
@@ -0,0 +1,31 @@
+const loginInputs = document.querySelectorAll(".login-input");
+const loginLabels = document.querySelectorAll(".login-label");
+
+const loginInputContainers = [
+  document.querySelector(".username-input-container"),
+  document.querySelector(".password-input-container"),
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < loginInputs.length; i++) {
+    loginInputs[i].addEventListener("focus", (event) => {
+      if (event.currentTarget === loginInputs[i]) {
+        loginLabels[i].style.transform = "translateY(-20px)";
+        loginLabels[i].style.fontSize = "14px";
+      }
+    });
+    loginInputs[i].addEventListener("blur", () => {
+      if (loginInputs[i].value === "") {
+        loginLabels[i].style.transform = "translateY(0px)";
+        loginLabels[i].style.fontSize = "16px";
+      }
+    });
+    loginInputs[i].addEventListener('keydown', (event) => {
+      if(event.key === 'ArrowDown') {
+        loginInputs[i+1].focus()
+      } else if(event.key === 'ArrowUp') {
+        loginInputs[i-1].focus()
+      }
+    })
+  }
+});
