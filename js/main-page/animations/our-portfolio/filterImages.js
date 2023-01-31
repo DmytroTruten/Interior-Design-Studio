69ba102a99cf3bdbee3156a41750c61119e7a127
DmitriyTruten <dmitriy2503a@gmail.com>
2023-01-31 18:35:12 +0200

    resolve bug with switching images within portfolio

--- js/main-page/animations/our-portfolio/filterImages.js
+++ js/main-page/animations/our-portfolio/filterImages.js
@@ -1,10 +1,13 @@
-export function filterPortfolioImages() {
-  const filters = document.querySelectorAll(".filter");
-  const galleryImages = Array.from(document.querySelectorAll("#gallery-img"));
-  const commercialImages = Array.from(
-    document.querySelectorAll("#commercial-img")
-  );
+const filters = document.querySelectorAll(".filter");
+const galleryImages = Array.from(document.querySelectorAll("#gallery-img"));
+const commercialImages = Array.from(
+  document.querySelectorAll("#commercial-img")
+);
 
+let currentImages = galleryImages;
+let nextImages;
+
+export function filterPortfolioImages() {
   for (let i = 0; i < filters.length; i++) {
     filters[i].addEventListener("click", function (event) {
       let activeFilter = document.getElementsByClassName("active-filter")[0];
@@ -17,29 +20,22 @@ export function filterPortfolioImages() {
       let previousImagesHideTime = 0;
       let displayNoneTime = 490;
       let currentImagesRevealTime = 500;
-      let currentImages = galleryImages;
-      let nextImages;
 
-      if (event.target.id === "all") {
-        nextImages = galleryImages;
-        currentImages = commercialImages
-        animateFilterImages(
-          currentImages,
-          nextImages,
-          previousImagesHideTime,
-          displayNoneTime,
-          currentImagesRevealTime
-          ) 
-        } else if (event.target.id === "commercial") {
-        nextImages = commercialImages;
-        animateFilterImages(
-          currentImages,
-          nextImages,
-          previousImagesHideTime,
-          displayNoneTime,
-          currentImagesRevealTime
-        ) 
+      switch (event.target.id) {
+        case "all":
+          nextImages = galleryImages;
+          break;
+        case "commercial":
+          nextImages = commercialImages;
+          break;
       }
+      animateFilterImages(
+        currentImages,
+        nextImages,
+        previousImagesHideTime,
+        displayNoneTime,
+        currentImagesRevealTime
+      );
     });
   }
 }
@@ -63,8 +59,15 @@ function animateFilterImages(
     setTimeout(() => {
       nextImages[i].style.animation = "reveal .5s ease-in-out";
       nextImages[i].style.display = "block";
+      if (i === 7) {
+        changeImages();
+      }
     }, currentImagesRevealTime);
     currentImagesRevealTime += 100;
   }
-  console.log(currentImages, nextImages)
+}
+
+function changeImages() {
+  currentImages = nextImages;
+  nextImages = undefined;
 }
