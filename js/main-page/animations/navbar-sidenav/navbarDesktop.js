0256610ff84c77ba94b5d988772ab06ff1411ee5
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-04 21:00:34 +0200

    add on hover animation for header navigation links

--- js/main-page/animations/navbar-sidenav/navbarDesktop.js
+++ js/main-page/animations/navbar-sidenav/navbarDesktop.js
@@ -1,9 +1,9 @@
 import { revealIntoDirection } from "../reveal-function/revealIntoDirection.js";
 export function animateNavbarDesktop() {
-  // Getting access to desktop navbar elements
-  const logo = document.querySelector(".logo");
 
+  // Getting access to desktop navbar elements
   let headerElements = [
+    document.querySelector(".logo"),
     Array.from(document.querySelectorAll(".header-link")),
     document.querySelector(".header-right-section").children[0],
     document.querySelector(".header-right-section").children[1],
@@ -12,13 +12,24 @@ export function animateNavbarDesktop() {
   // Declare variable for setTimeout
   let revealTime = 200;
 
+  // console.log(headerElements)
+
   // All elements of nested sub-arrays have been "raised" recursively
   headerElements = headerElements.flat();
 
+  for (let i = 0; i < headerElements.length; i++) {
+    if(headerElements[i].classList.contains("header-link")) {
+      headerElements[i].addEventListener('mouseenter', () => {
+        headerElements[i].style.transform = 'scale(1.1)';
+      })
+      headerElements[i].addEventListener('mouseleave', () => {
+        headerElements[i].style.transform = 'scale(1)';
+      })
+    }
+  }
+
   /* For each navbar element applying 
   animation in certain intervals */
-  revealIntoDirection(logo, "left");
-
   for (let i = 0; i < headerElements.length; i++) {
     setTimeout(() => {
       revealIntoDirection(headerElements[i], "left");
