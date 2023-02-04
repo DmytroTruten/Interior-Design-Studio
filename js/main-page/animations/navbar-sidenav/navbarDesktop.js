5eb21c17259aa57c6903974995627d0772a6bcfc
DmitriyTruten <dmitriy2503a@gmail.com>
2023-02-04 20:41:41 +0200

    refactor animation of desktop navbar

--- js/main-page/animations/navbar-sidenav/navbarDesktop.js
+++ js/main-page/animations/navbar-sidenav/navbarDesktop.js
@@ -2,26 +2,27 @@ import { revealIntoDirection } from "../reveal-function/revealIntoDirection.js";
 export function animateNavbarDesktop() {
   // Getting access to desktop navbar elements
   const logo = document.querySelector(".logo");
-  const headerLink = document.querySelectorAll(".header-link");
-  const headerRightSection = document.querySelector(".header-right-section");
 
-  /* Separately for each navbar element applying 
+  let headerElements = [
+    Array.from(document.querySelectorAll(".header-link")),
+    document.querySelector(".header-right-section").children[0],
+    document.querySelector(".header-right-section").children[1],
+  ];
+
+  // Declare variable for setTimeout
+  let revealTime = 200;
+
+  // All elements of nested sub-arrays have been "raised" recursively
+  headerElements = headerElements.flat();
+
+  /* For each navbar element applying 
   animation in certain intervals */
   revealIntoDirection(logo, "left");
 
-  setTimeout(() => {
-    revealIntoDirection(headerLink[0], "left");
-  }, 200);
-  setTimeout(() => {
-    revealIntoDirection(headerLink[1], "left");
-  }, 400);
-  setTimeout(() => {
-    revealIntoDirection(headerLink[2], "left");
-  }, 600);
-  setTimeout(() => {
-    revealIntoDirection(headerRightSection.children[0], "left");
-  }, 800);
-  setTimeout(() => {
-    revealIntoDirection(headerRightSection.children[1], "left");
-  }, 1000);
+  for (let i = 0; i < headerElements.length; i++) {
+    setTimeout(() => {
+      revealIntoDirection(headerElements[i], "left");
+    }, revealTime);
+    revealTime += 200;
+  }
 }
